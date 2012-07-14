8673cf8b9f4b170fb4e56395b11e02e27e75c59d
lintile <aaron@lintile.com>
2012-07-13 17:36:41 -0700

    Solution to problem 19

--- 019.pl
+++ 019.pl
@@ -1,8 +1,18 @@
 use DateTime;
 
+$numSumdays = 0;
+
 $dt = DateTime->new(
-      year       => 1900,
+      year       => 1901,
       month      => 1,
       day        => 1);
 
-print $dt->day_of_week() . "\n";
+$dt->add( days => 1 ) while ( $dt->day_of_week() != 7);
+
+while ( $dt->year != 2001 ) {
+	$numSumdays++ if ( $dt->day == 1 );	#add if it's the first of the month
+	$dt->add( days => 7 ); 			#go to the next sunday
+	#print $dt->mdy('/') . "\n";
+}
+
+print $numSumdays . "\n";
